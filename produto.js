5eac5ae868bcb39171b527b3b1fd2a53204b2575
coly1090 <ferreira.emyly@escola.pr.gov.br>
2025-10-29 21:16:56 -0300

    Add files via upload

---
+++ produto.js
@@ -0,0 +1,9 @@
+const nomedoproduto="camiseta de algodão";
+let precodoproduto=29.99;
+const marcadoproduto="marca x"
+const produtocompleto=(nomedoproduto+":"+"\n"+marcadoproduto+":"+"\n"+precodoproduto);
+precodoproduto=49.00
+console.log(produtocompleto)
+console.log("produto:",produtocompleto);
+console.log("preco atualizado: R$",precodoproduto);
+nomedoproduto="camiseta de linho"
